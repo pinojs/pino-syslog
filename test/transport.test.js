d800dddc890362712cb6027cfb8f3705c781055c
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-09-27 09:37:38 +0200

    update path

--- test/transport.test.js
+++ test/transport.test.js
@@ -9,7 +9,7 @@ const { spawnSync } = require('child_process')
 
 const { test } = require('tap')
 const pino = require('pino')
-
+const pinoSyslog = join(__dirname, '..', 'lib', 'transport.js')
 const timeout = promisify(setTimeout)
 
 const messages = require(join(__dirname, 'fixtures', 'messages'))
@@ -29,7 +29,7 @@ test('syslog pino transport test rfc3164', async t => {
   }
 
   const transport = pino.transport({
-    target: join(__dirname, '../app.js'),
+    target: pinoSyslog,
     level: 'info',
     options: sysLogOptions
   })
@@ -59,7 +59,7 @@ test('syslog pino transport test rfc5424', async t => {
   }
 
   const transport = pino.transport({
-    target: join(__dirname, '../app.js'),
+    target: pinoSyslog,
     level: 'info',
     options: sysLogOptions
   })
@@ -83,12 +83,12 @@ test('tock pino transport test stdout', async t => {
   const result = spawnSync('node', [join(__dirname, 'fixtures', 'log-stdout.js'), '1'], {
     cwd: process.cwd()
   })
-  t.equal(result.output[1].toString().trim(), '<134>1 2016-04-01T16:44:58Z MacBook-Pro-3 - 94473 - - hello world')
+  t.equal(result.stdout.toString().trim(), '<134>1 2016-04-01T16:44:58Z MacBook-Pro-3 - 94473 - - hello world')
 })
 
 test('tock pino transport test stderr', async t => {
   const result = spawnSync('node', [join(__dirname, 'fixtures', 'log-stdout.js'), '2'], {
     cwd: process.cwd()
   })
-  t.equal(result.output[2].toString().trim(), '<134>1 2016-04-01T16:44:58Z MacBook-Pro-3 - 94473 - - hello world')
+  t.equal(result.stderr.toString().trim(), '<134>1 2016-04-01T16:44:58Z MacBook-Pro-3 - 94473 - - hello world')
 })
