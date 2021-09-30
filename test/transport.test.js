1ddd51f20f04bd4a2e608145a784d012381ea978
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-09-30 18:36:05 +0200

    fix node 12 failing

--- test/transport.test.js
+++ test/transport.test.js
@@ -116,6 +116,7 @@ test('pino pipeline', t => {
       t.teardown(() => {
         serverSocket.close()
         serverSocket.unref()
+        transport.end()
       })
 
       const address = serverSocket.address().address
