f1f075e2bc924f91a40bf4b8818555cab86ff983
Matteo Collina <hello@matteocollina.com>
2023-01-11 15:19:10 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/transport.test.js
+++ test/transport.test.js
@@ -118,7 +118,6 @@ test('pino pipeline', t => {
       t.teardown(() => {
         serverSocket.close()
         serverSocket.unref()
-        transport.end()
       })
 
       const address = serverSocket.address().address
