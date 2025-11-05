188f835aab131b5483eca010e31637678d072502
Doug Kasten <dkasten@juniper.net>
2025-11-05 10:26:03 -0700

    Revert version change and RFC3164 timestamp changes; update all tests using RFC5424 to include milliseconds in timestamp based on current test messages (#30)

--- test/transport.test.js
+++ test/transport.test.js
@@ -8,7 +8,7 @@ test('syslog pino transport test stdout', async t => {
   const result = spawnSync('node', ['--no-warnings', join(__dirname, 'fixtures', 'log-stdout.js'), '1'], {
     cwd: process.cwd()
   })
-  t.equal(result.stdout.toString().trim(), '<134>1 2016-04-01T16:44:58Z MacBook-Pro-3 - 94473 - - hello world')
+  t.equal(result.stdout.toString().trim(), '<134>1 2016-04-01T16:44:58.958Z MacBook-Pro-3 - 94473 - - hello world')
   t.equal(result.status, 0)
 })
 
@@ -16,6 +16,6 @@ test('syslog pino transport test stderr', async t => {
   const result = spawnSync('node', ['--no-warnings', join(__dirname, 'fixtures', 'log-stdout.js'), '2'], {
     cwd: process.cwd()
   })
-  t.equal(result.stderr.toString().trim(), '<134>1 2016-04-01T16:44:58Z MacBook-Pro-3 - 94473 - - hello world')
+  t.equal(result.stderr.toString().trim(), '<134>1 2016-04-01T16:44:58.958Z MacBook-Pro-3 - 94473 - - hello world')
   t.equal(result.status, 0)
 })
